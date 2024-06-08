5252dab0b1340811a20fda591459ebc2d5eb1b79
Numan Ahmed <numanahmedmail@gmail.com>
2024-06-09 02:33:46 +0500

    side bar and navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
+import Home from './pages/Home/Home'
+import Video from './pages/Video/Video'
+import { Routes,Route } from 'react-router-dom'
 
 const App = () => {
+  const [sidebar,setSidebar] = useState(true);
   return (
     <div>
-      <div>dkajsdkjaskjdkajsdkjk</div>
-      <Navbar />
+      <Navbar setSidebar={setSidebar} />
+      <Routes>
+        <Route path="/" element={<Home sidebar={sidebar}/>} />
+        <Route path="/video/:categoryId/:videoId" element={<Video/>} />
+      </Routes>
     </div>
   )
 }
